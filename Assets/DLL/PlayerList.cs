73445f5aca5d4b0bf0433b96d3a0a73978d7a535
Alex <alexander052702@gmail.com>
2024-05-10 00:02:28 -0400

    camera rects

--- Assets/DLL/PlayerList.cs
+++ Assets/DLL/PlayerList.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class PlayerList : SingletonMonobehaviour<PlayerList>
 {
+    [SerializeField] PlayerSpawnSystem playerSpawnSystem;
     [SerializeField] GameObject[] characters;
     [SerializeField] GameObject bodyParent;
 
     int counter;
     [SerializeField] GameObject[] spawnPositions;
 
+    /// <summary>
+    /// Spawns Character Body based on ID of character
+    /// </summary>
+    /// <param name="characterID"></param>
+    /// <returns>
+    /// Returns Player Main script on body
+    /// </returns>
     public PlayerMain SpawnCharacterBody(int characterID)
     {
         GameObject character = Instantiate(characters[characterID], spawnPositions[counter++].transform.position, Quaternion.identity);
         character.transform.parent = bodyParent.transform;
 
         PlayerMain playerMain = character.GetComponent<PlayerMain>();
+
+        playerSpawnSystem.AddPlayerBody(playerMain);
+
         return playerMain;
     }
 }
--- Assets/DLL/PlayerSpawnSystem.cs
+++ Assets/DLL/PlayerSpawnSystem.cs
@@ -24,6 +24,11 @@ public class PlayerSpawnSystem : SingletonMonobehaviour<PlayerSpawnSystem>
         }
     }
 
+    [Header("Spawned Player Bodies")]
+    [SerializeField] List<PlayerMain> spawnedBodies;
+    public void AddPlayerBody(PlayerMain body) { spawnedBodies.Add(body); UpdatePlayerCameraRects();}
+    public void RemovePlayerBody(PlayerMain body) { spawnedBodies.Remove(body); UpdatePlayerCameraRects(); }
+
     /// <summary>
     /// Checks the amount of players and returns true if another player can spawn
     /// </summary>
@@ -52,5 +57,72 @@ public class PlayerSpawnSystem : SingletonMonobehaviour<PlayerSpawnSystem>
             if(body.GetBodyDeviceID() == deviceId) { return body; }
         }
         return null;
+    }
+
+    Rect[] cameraRects;
+
+    ///<summary>
+    /// Updates the camera rects on all players in scenes
+    ///</summary>
+    private void UpdatePlayerCameraRects()
+    {
+        cameraRects = CalculateRects();
+
+        if (cameraRects.Length <= 0)
+            return;
+
+        int cameraRectCounter = 0;
+
+        for (int i = 0; i < spawnedBodies.Count; i++)
+        {
+            if (spawnedBodies[i] != null)
+            {
+                if (cameraRectCounter < cameraRects.Length)
+                {
+                    Rect temp = cameraRects[cameraRectCounter];
+                    spawnedBodies[i].playerCamera.rect = temp;
+                    cameraRectCounter++;
+                }
+                else
+                {
+                    // Handle the case where there are more non-null player inputs than camera rects
+                    break;
+                }
+            }
+        }
+    }
+
+    ///<summary>
+    /// Calculates the camera rects for when there are 1 - 4 players
+    ///</summary>
+    private Rect[] CalculateRects()
+    {
+        Rect[] viewportRects = new Rect[playerCount];
+
+        // 1 Player
+        if (playerCount == 1)
+        {
+            viewportRects[0] = new Rect(0, 0, 1, 1);
+        }
+        else if (playerCount == 2)
+        {
+            viewportRects[0] = new Rect(0.25f, 0.5f, 0.5f, 0.5f);
+            viewportRects[1] = new Rect(0.25f, 0, 0.5f, 0.5f);
+        }
+        else if (playerCount == 3)
+        {
+            viewportRects[0] = new Rect(0, 0.5f, 0.5f, 0.5f);
+            viewportRects[1] = new Rect(0.5f, 0.5f, 0.5f, 0.5f);
+            viewportRects[2] = new Rect(0.25f, 0, 0.5f, 0.5f);
+        }
+        else if (playerCount == 4)
+        {
+            viewportRects[0] = new Rect(0, 0.5f, 0.5f, 0.5f);
+            viewportRects[1] = new Rect(0.5f, 0.5f, 0.5f, 0.5f);
+            viewportRects[2] = new Rect(0, 0, 0.5f, 0.5f);
+            viewportRects[3] = new Rect(0.5f, 0, 0.5f, 0.5f);
+        }
+
+        return viewportRects;
     }
 }
