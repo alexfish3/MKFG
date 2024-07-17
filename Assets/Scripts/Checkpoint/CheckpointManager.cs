9ddfe8b04177aea06f8614d4696b07b72a2b96da
alexfish3 <alexander052702@gmail.com>
2024-07-17 00:08:48 -0400

    Tie

--- Assets/Scripts/Checkpoint/CheckpointManager.cs
+++ Assets/Scripts/Checkpoint/CheckpointManager.cs
@@ -17,7 +17,7 @@ public class CheckpointManager : SingletonMonobehaviour<CheckpointManager>
     [SerializeField] private int totalLaps = 3;
 
     [Header("Game Information")]
-    [SerializeField] private float tieDistance = 10f;
+    private float tieDistance = 3f;
 
     private Checkpoint[] checkpoints;
     private int maxLap = 0; // highest running lap, so if the player in first is on lap 2 this value will be 2
