70abd1dfff28fe63f8eaf042738c74c761de82cb
Max Moverley <56087540+mmoverle2842@users.noreply.github.com>
2024-07-23 21:59:22 -0400

    NEW MAP

---
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float smoothSpeed;
+    [SerializeField] private Vector3 offset;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (target == null)
+            return;
+
+        Vector3 desiredPos = target.position + offset;
+
+        Vector3 smoothPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
+
+        transform.position = smoothPos;
+
+        transform.LookAt(target);
+    }
+}
