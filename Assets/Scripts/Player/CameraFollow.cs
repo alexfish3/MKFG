5c9a17518b3d1ca2142b63b84941441a6bd374ee
Cole Bereskin <cole.bereskin@gmail.com>
2024-08-07 00:36:55 -0400

    Updated Camera Kart Follow

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -4,22 +4,21 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField] private Transform target;
+    [SerializeField] private GameObject target;
+    [SerializeField] private PlayerMain player;
     [SerializeField] private float smoothSpeed;
-    [SerializeField] private Vector3 offset;
+    [SerializeField] private float smoothrotation;
+    private bool collisionDetected = false;
 
     // Update is called once per frame
-    void LateUpdate()
+    void FixedUpdate()
     {
         if (target == null)
             return;
 
-        Vector3 desiredPos = target.position + offset;
+        //transform.LookAt(player.kart.transform);
+        transform.rotation = Quaternion.Slerp(transform.rotation, target.transform.rotation, smoothSpeed);
+        transform.position = Vector3.Slerp(transform.position, target.transform.position, smoothSpeed);
 
-        Vector3 smoothPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
-
-        transform.position = smoothPos;
-
-        transform.LookAt(target);
     }
 }
