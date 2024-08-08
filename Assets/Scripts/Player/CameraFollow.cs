f9f67d32a831717d12963eb787d9dd43062bb5e6
Cole Bereskin <cole.bereskin@gmail.com>
2024-08-07 22:32:47 -0400

    Fixed Camera Follow

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -8,9 +8,11 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private GameObject target;
     [SerializeField] private Camera cam;
     [SerializeField] private PlayerMain player;
-    private float smoothSpeed = 50;
-    private float smoothrotation = 50;
+    private float smoothSpeed = 30;
+    private float zoomSpeed = 3f;
+    private float smoothrotation = 30;
     private bool collisionDetected = false;
+    [SerializeField] Vector3 oldCamPos;
 
     // Update is called once per frame
     void FixedUpdate()
@@ -24,7 +26,6 @@ public class CameraFollow : MonoBehaviour
         transform.rotation = Quaternion.Lerp(transform.rotation, target.transform.rotation, smoothSpeed * Time.fixedDeltaTime);
         transform.position = Vector3.Lerp(transform.position, target.transform.position, smoothSpeed * Time.fixedDeltaTime);
 
-
         //Collider[] hitcollider;
         //hitcollider = Physics.OverlapSphere(cam.transform.position, 0.25f);
 
@@ -32,8 +33,20 @@ public class CameraFollow : MonoBehaviour
         {
             transform.position = oldPos;
             transform.rotation = oldRot;
+
+            if (Physics.CheckSphere(transform.position, 1f)) 
+            { 
+                transform.position += -cam.transform.forward * zoomSpeed * Time.fixedDeltaTime;
+
+                //transform.rotation = Quaternion.Lerp(transform.rotation, cam.transform.rotation, smoothSpeed * Time.fixedDeltaTime);
+            } else
+            {
+                return;
+            }
+            //cam.transform.localPosition = Vector3.Lerp(cam.transform.localPosition, Vector3.zero, zoomSpeed * Time.fixedDeltaTime);
             return;
-        }
+        } 
+        //cam.transform.localPosition = Vector3.Lerp(cam.transform.localPosition, oldCamPos, zoomSpeed * Time.fixedDeltaTime);
 
         //transform.LookAt(player.kart.transform);
 
