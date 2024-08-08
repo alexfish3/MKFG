bfd8783217f3ee72924dcd7b2f5bb9f752a4d504
Cole Bereskin <cole.bereskin@gmail.com>
2024-08-07 21:30:44 -0400

    FIXED CAMERAAAAA

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private GameObject target;
+    [SerializeField] private Camera cam;
     [SerializeField] private PlayerMain player;
-    private float smoothSpeed = 10;
-    private float smoothrotation = 10;
+    private float smoothSpeed = 50;
+    private float smoothrotation = 50;
     private bool collisionDetected = false;
 
     // Update is called once per frame
@@ -16,9 +18,30 @@ public class CameraFollow : MonoBehaviour
         if (target == null)
             return;
 
-        //transform.LookAt(player.kart.transform);
-        transform.rotation = Quaternion.Slerp(transform.rotation, target.transform.rotation, smoothSpeed);
-        transform.position = Vector3.Slerp(transform.position, target.transform.position, smoothSpeed);
+        Vector3 oldPos = transform.position;
+        Quaternion oldRot = transform.rotation;
 
+        transform.rotation = Quaternion.Lerp(transform.rotation, target.transform.rotation, smoothSpeed * Time.fixedDeltaTime);
+        transform.position = Vector3.Lerp(transform.position, target.transform.position, smoothSpeed * Time.fixedDeltaTime);
+
+
+        //Collider[] hitcollider;
+        //hitcollider = Physics.OverlapSphere(cam.transform.position, 0.25f);
+
+        if (Physics.CheckSphere(cam.transform.position, 1f))
+        {
+            transform.position = oldPos;
+            transform.rotation = oldRot;
+            return;
+        }
+
+        //transform.LookAt(player.kart.transform);
+
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(cam.transform.position, 1);
     }
 }
